f49d4de886956c747ad6677a3717fbc1ed8bb9f0
dayton072 <dayton.kurniawan@gmail.com>
2026-08-30 14:30:22 +0700

    Komitmen saya

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -1,12 +1,17 @@
 import { test, expect } from '@playwright/test';
 
 test('login invalid', async ({ page }) => {
+  //pre
   await page.goto('https://www.emra.chat/login');
+
+  //steps
   await page.getByRole('textbox', { name: 'Email' }).click();
   await page.getByRole('textbox', { name: 'Email' }).fill('test@email.com');
   await page.getByRole('textbox', { name: 'Email' }).press('Tab');
   await page.getByRole('textbox', { name: 'Password' }).click();
   await page.getByRole('textbox', { name: 'Password' }).fill('test123');
   await page.getByRole('button', { name: 'Sign In' }).click();
+
+  //expect
   await expect(page.getByRole('listitem')).toBeVisible();
 });
